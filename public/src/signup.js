bd73e84bee0a6d8277732efb22f42ec9c3dbe60d
Christine Chee <christinechee88@hotmail.com>
2021-04-30 19:57:57 +0100

    Add event listener for user sign up and render welcome message after signed up

--- public/src/signup.js
+++ public/src/signup.js
@@ -1,3 +1,47 @@
-$(document).ready(function () {
-  
-});
+class User {
+  constructor(id, handle) {
+    this.id = id;
+    this.handle = handle;
+  }
+}
+
+let user;
+function eventListeners() {
+  document.addEventListener("DOMContentLoaded", signUpListener()); 
+}
+
+eventListeners();
+
+
+function signUpListener() {
+  let signUpButton = document.getElementById("signupButton");
+  signUpButton.addEventListener("click", function (event) {
+  event.preventDefault(); 
+  let handle = document.getElementsByName("handle")[0].value;
+  let password = document.getElementsByName("password")[0].value;
+  let data = {"user": {"handle":`${handle}`, "password":`${password}`}}
+  console.log(data);
+  signup(data)
+  });
+}
+
+function signup(data) {
+  fetch("https://chitter-backend-api-v2.herokuapp.com/users", {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    user = new User(data.id, data.handle);
+    document.getElementsByClassName("signupcontainer")[0].innerHTML = 
+    `<h2>Welcome ${user.handle}</h2>`
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+}
+
